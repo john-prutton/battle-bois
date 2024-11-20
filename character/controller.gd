1bf9db5b9768272c46821ab20e3cdd9915be8028
John P <johnrprutton@gmail.com>
2024-11-20 23:55:20 +0200

    feat: add basic movement

--- character/controller.gd
+++ character/controller.gd
@@ -1,9 +1,29 @@
 extends CharacterBody3D
 
 func _process(delta: float) -> void:
-	if !is_on_floor():
-		velocity.y += -1 * delta
+	var input = Vector3(
+		Input.get_axis("walk_left", "walk_right"),
+		0,
+		Input.get_axis("walk_up", "walk_down")
+	)
+	
+	if input.length() != 0:
+		input.limit_length(1)
+	elif velocity.length() > 0.25:
+		input = -0.25 * velocity
+		input.y = 0
 	else:
-		velocity.y = 0
-		
+		input = -velocity
+		input.y = 0
+	
+	var magnitudes_multiplied = input.length() * velocity.length()
+	var direction_difference_multiplier = 1 + max(
+		-(input.dot(velocity) / magnitudes_multiplied) if magnitudes_multiplied != 0 else 0,
+		0
+	)
+	
+	velocity += 50 * direction_difference_multiplier * delta * input
+	velocity.y = (velocity.y + -1 * delta) if !is_on_floor() else 0
+	velocity = velocity.limit_length(10)
+	
 	move_and_slide()
